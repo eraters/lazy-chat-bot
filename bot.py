f6d16260587ffe61b2b01b9ecd602b823b191d6e
imlazywithazismyname <54771596+imlazywithazismyname@users.noreply.github.com>
2019-12-04 18:57:04 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,25 @@
 import asyncio
 
 print('Hello! This is a chat bot made by ImLazyWithAZ')
+
+print('Hello! What is your name?')
+
+def name(name):
+  print('Hi ' +name)
+  
+name(input(''))
+
+print('How are you?')
+
+def hru(name):
+  print('Oh, you are ' +name)
+  print('I am good!')
+  
+print('Why are you talking to me?')
+  
+hru(input(''))
+
+def wyn(name):
+  print('Oh, OK')
+  
+wyn(input(''))
